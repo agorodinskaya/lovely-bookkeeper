438991db89ba480816c8ebea529e015581638ca7
Anastasia Gorodinskaya <agorodinskaya@gmail.com>
2021-03-22 09:15:16 +1100

    comp header and balance

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Header from "./components/Header";
+import Balance from "./components/Balance";
+function App() {
+  return (
+    <div className='App'>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Balance from "./Balance";
+const Header = () => {
+  return (
+    <>
+      <h2>Lovely bookkeeper</h2>
+      <div class='container'>
+        <Balance />
+      </div>
+    </>
+  );
+};
+
+export default Header;
